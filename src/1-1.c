46cc2edcdaa8085220d73d65794f42cc7c4e1687
kumataiyo510 <kumataiyo510@yahoo.co.jp>
2021-03-04 12:21:10 +0900

    first commit

---
+++ src/1-1.c
@@ -0,0 +1,40 @@
+// 文字列等の表示と変数について
+
+#include <stdio.h>
+
+/* int main(void){
+    printf("%d\n", 15 + 37);    //print format
+    return 0;
+} */
+
+/* int main(void){
+    printf("15と37の和は%dです。\n", 15 + 37);
+    return 0;
+} */
+
+/* int main(void){
+    printf("15から37を引いた値は%dです。\n", 15 - 37);
+    return 0;
+} */
+
+/* int main(void){
+    printf("もしもし\n\nこんにちは。\nそれでは。\n");
+    return 0;
+} */
+
+/* int main(void){
+    int vx, vy;
+    vx = 57;
+    vy = vx + 10;
+    
+    printf("vxの値は%dです。\n", vx);
+    printf("vyの値は%dです。\n", vy);
+
+    return 0;
+} */
+
+/* int main(void){
+    int a = 3.14;
+    printf("aの値は%dです。\n", a);
+    return 0;
+} */
